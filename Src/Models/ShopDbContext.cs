672613af5a07f4eb2b707b6b08c875ea0849c83e
zolotonos <zolotonos.ua@gmail.com>
2025-12-17 16:41:22 +0200

    Test!

--- Src/Models/ShopDbContext.cs
+++ Src/Models/ShopDbContext.cs
@@ -32,9 +32,13 @@ public partial class ShopDbContext : DbContext
     public virtual DbSet<Supplier> Suppliers { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=shop_db;Username=admin;Password=password123");
-
+    {
+       
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=shop_db;Username=admin;Password=password123");
+        }
+    }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Address>(entity =>
---
+++ Tests/OrderServiceTests.cs
@@ -0,0 +1,83 @@
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics; 
+using Kurs_db.Models;
+using Kurs_db.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class OrderServiceTests
+    {
+        private ShopDbContext GetInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ShopDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            var context = new ShopDbContext(options);
+            return context;
+        }
+
+        [Fact]
+        public async Task CreateOrderAsync_Should_ReduceStock_And_CalculateTotal()
+        {
+            // 1. Arrange
+            using var context = GetInMemoryDbContext();
+            
+            var customerId = Guid.NewGuid();
+            var productId = Guid.NewGuid();
+            
+            context.Customers.Add(new Customer { CustomerId = customerId, FirstName = "Test", LastName = "User", Email = "test@test.com", Phone = "123" });
+            context.Products.Add(new Product { ProductId = productId, Name = "Laptop", Price = 1000, StockQuantity = 10, IsDeleted = false });
+            await context.SaveChangesAsync();
+
+            var service = new OrderService(context);
+            
+            var requestItems = new Dictionary<Guid, int>
+            {
+                { productId, 2 }
+            };
+
+            // 2. Act
+            var order = await service.CreateOrderAsync(customerId, Guid.NewGuid(), requestItems);
+
+            // 3. Assert
+            Assert.NotNull(order);
+            Assert.Equal(2000, order.TotalAmount);
+            
+            var productInDb = await context.Products.FindAsync(productId);
+            Assert.NotNull(productInDb);
+            Assert.Equal(8, productInDb!.StockQuantity);
+        }
+
+        [Fact]
+        public async Task CreateOrderAsync_Should_Fail_If_NotEnoughStock()
+        {
+            // 1. Arrange
+            using var context = GetInMemoryDbContext();
+            var customerId = Guid.NewGuid();
+            var productId = Guid.NewGuid();
+
+            context.Customers.Add(new Customer { CustomerId = customerId, FirstName = "Test", LastName = "User", Email = "test@test.com", Phone="123" });
+            context.Products.Add(new Product { ProductId = productId, Name = "Laptop", Price = 1000, StockQuantity = 5, IsDeleted = false });
+            await context.SaveChangesAsync();
+
+            var service = new OrderService(context);
+
+            var requestItems = new Dictionary<Guid, int>
+            {
+                { productId, 10 }
+            };
+
+            // 2. Act & Assert
+            await Assert.ThrowsAsync<Exception>(async () => 
+            {
+                await service.CreateOrderAsync(customerId, Guid.NewGuid(), requestItems);
+            });
+        }
+    }
+}
